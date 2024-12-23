cbf7027cf9e99d73268c5b60aefdf3abd9386c23
Alisson Alves <105590677+aas1989@users.noreply.github.com>
2024-12-23 11:32:30 -0300

    Condicionais

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,11 +1,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 26.0;
+        double valorSolicitado = 22.0;
 
-        if(valorSolicitado < saldo)
-        saldo = saldo - valorSolicitado;
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo R$ " + saldo);
+        }else
+        System.out.println("Saldo insuficiente");
 
-        System.out.println(saldo);
     }
 }
