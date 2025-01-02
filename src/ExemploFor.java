226461202d5add858dda5d73ddc1454fc9e10e56
Alisson Alves <105590677+aas1989@users.noreply.github.com>
2025-01-02 08:42:43 -0300

    for

---
+++ src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for( int carneirinho = 1; carneirinho <= 20; carneirinho ++) {
+            System.out.println("Contando carnerinhos " + carneirinho);
+        }
+
+        System.out.println("João dormiu!");
+    }
+}
